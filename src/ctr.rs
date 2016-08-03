32ff57f52494f940b5e075ccc2112c0f0747946d
WalrusCow <willgmcd@gmail.com>
2016-08-03 19:02:52 +0100

    Some lifetime issues...

--- src/ctr.rs
+++ src/ctr.rs
@@ -2,7 +2,6 @@ use aes::AES;
 
 type ByteIterator = Iterator<Item=u8>;
 
-/*
 pub struct CTR {
     aes: AES,
 }
@@ -15,97 +14,76 @@ impl CTR {
     }
 
     /// Handily, CTR encryption is the same as decryption
-    fn encrypt_or_decrypt(&self, nonce: &Vec<u8>, data: &mut ByteIterator) -> CTR_Iterator {
+    fn encrypt_or_decrypt<'data, 'own>(
+        &'own self,
+        nonce: &Vec<u8>,
+        data: &'data mut ByteIterator
+    ) -> CTR_Iterator<'data, 'own> {
         // Must be shorter than a block
         assert!(nonce.len() <= 16);
 
         let mut nonce_block = [0u8; 16];
-        for (i, b) in &nonce.iter().enumerate() {
-            nonce_block[i] = b;
+        for (i, b) in nonce.iter().enumerate() {
+            nonce_block[i] = *b;
         }
 
-        CTR_Iterator::new(nonce_block, &data, &self.aes)
+        CTR_Iterator::new(nonce_block, &mut data, &self.aes)
     }
 
-    pub fn encrypt(&self, nonce: &Vec<u8>, plaintext: &mut ByteIterator) -> CTR_Iterator {
-        self.encrypt_or_decrypt(&nonce, &plaintext)
+    pub fn encrypt<'data, 'own>(
+        &'own self,
+        nonce: &Vec<u8>,
+        plaintext: &'data mut ByteIterator
+    ) -> CTR_Iterator<'data, 'own> {
+        self.encrypt_or_decrypt(&nonce, &mut plaintext)
     }
 
-    pub fn decrypt(&self, nonce: &Vec<u8>, ciphertext: &mut ByteIterator) -> CTR_Iterator {
-        self.encrypt_or_decrypt(&nonce, &ciphertext)
+    pub fn decrypt<'data, 'own>(
+        &'own self,
+        nonce: &Vec<u8>,
+        ciphertext: &'data mut ByteIterator
+    ) -> CTR_Iterator<'data, 'own> {
+        self.encrypt_or_decrypt(&nonce, &mut ciphertext)
     }
 
 }
-*/
-/*
 
 // TODO: How to do lifetimes??
-pub struct CTR_Iterator {
-    data: &'a mut ByteIterator,
+pub struct CTR_Iterator<'caller, 'parent> {
+    data: &'caller mut ByteIterator,
     nonce_block: [u8; 16],
     // This is the encrypted nonce block
     block: [u8; 16],
     // Which byte in the encrypted block to use next for the stream
     next_block_byte: usize,
     // The AES thingy to use
-    aes: &'b AES,
+    aes: &'parent AES,
 }
 
-impl CTR_Iterator {
-    fn new(nonce_block: [u8; 16], data: &'a mut ByteIterator, aes: &'b AES) -> CTR_Iterator {
+impl<'caller, 'parent> CTR_Iterator<'caller, 'parent> {
+    fn new<'c, 'p>(
+        nonce_block: [u8; 16],
+        data: &'c mut ByteIterator,
+        aes: &'p AES
+    ) -> CTR_Iterator<'c, 'p> {
         CTR_Iterator {
-            data: &data,
+            data: &mut data,
             nonce_block: nonce_block,
             block: [0u8; 16],
             next_block_byte: 0,
             aes: &aes,
         }
     }
-
-    // TODO: Here are some ideas, we need to test it thoroughly lol
-    fn increment_nonce_block(&mut self) {
-        /*
-        // Do magic things
-        if let Some((i, b)) = nonce_block.iter_mut().enumerate().rev().skip_while(|x| x == 0xff).next() {
-            // We matched
-            *b += 1;
-            for j in i+1..16 {
-                nonce_block[j] = 0;
-            }
-        }
-        else {
-            // The whole block is 0xff
-            for b in nonce_block.iter_mut() {
-                *b = 0;
-            }
-        }
-
-        for i in (0..16).rev() {
-            if nonce_block[i] != 0xff {
-                nonce_block[i] += 1;
-                for j in (i + 1)..16 {
-                    nonce_block[j] = 0;
-                }
-                break
-            } else if i == 15 {
-                // Everything was 0xff
-                for j in 0..16 {
-                    nonce_block[j] = 0;
-                }
-            }
-        }
-        */
-    }
 }
 
-impl Iterator for CTR_Iterator {
+impl<'caller, 'parent> Iterator for CTR_Iterator<'caller, 'parent> {
     type Item = u8;
 
     fn next(&mut self) -> Option<u8> {
         if self.next_block_byte == 16 {
             // We must encrypt the next block.
             // Increment nonce by one, encrypt it under the aes key
-            self.increment_nonce_block();
+            increment_byte_array(&mut self.nonce_block);
             self.block = self.aes.encrypt_block(&self.nonce_block);
             self.next_block_byte = 0;
         }
@@ -118,9 +96,8 @@ impl Iterator for CTR_Iterator {
         }
     }
 }
-*/
 
-pub fn increment_byte_array(byte_array: &mut [u8]) -> () {
+fn increment_byte_array(byte_array: &mut [u8]) -> () {
     // Start at len and go to zero to avoid negative overflow on usize
     let mut idx = byte_array.len();
     while idx > 0 && byte_array[idx - 1] == 0xff {
@@ -132,35 +109,35 @@ pub fn increment_byte_array(byte_array: &mut [u8]) -> () {
     }
 }
 
+// Tests for private function
 #[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn short_array_increment() {
+    let mut arr = [0u8];
+    increment_byte_array(&mut arr);
+    assert_eq!(arr, [1u8]);
+}
 
-    #[test]
-    fn short_array_increment() {
-        let mut arr = [0u8];
-        increment_byte_array(&mut arr);
-        assert_eq!(arr, [1u8]);
-    }
+#[cfg(test)]
+#[test]
+fn short_array_increment_overflow() {
+    let mut arr = [0xffu8];
+    increment_byte_array(&mut arr);
+    assert_eq!(arr, [0u8]);
+}
 
-    #[test]
-    fn short_array_increment_overflow() {
-        let mut arr = [0xffu8];
-        increment_byte_array(&mut arr);
-        assert_eq!(arr, [0u8]);
-    }
+#[cfg(test)]
+#[test]
+fn long_array_increment() {
+    let mut arr = [1u8, 0xffu8];
+    increment_byte_array(&mut arr);
+    assert_eq!(arr, [2u8, 0u8]);
+}
 
-    #[test]
-    fn long_array_increment() {
-        let mut arr = [1u8, 0xffu8];
-        increment_byte_array(&mut arr);
-        assert_eq!(arr, [2u8, 0u8]);
-    }
-
-    #[test]
-    fn long_array_increment_overflow() {
-        let mut arr = [0xffu8, 0xffu8, 0xffu8];
-        increment_byte_array(&mut arr);
-        assert_eq!(arr, [0u8, 0u8, 0u8]);
-    }
+#[cfg(test)]
+#[test]
+fn long_array_increment_overflow() {
+    let mut arr = [0xffu8, 0xffu8, 0xffu8];
+    increment_byte_array(&mut arr);
+    assert_eq!(arr, [0u8, 0u8, 0u8]);
 }
